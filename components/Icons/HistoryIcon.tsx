a09e68d02cee038e6a1129d33d46451acebb422b
QuangTran <quang.tranvics22@hcmut.edu.vn>
2023-12-28 16:29:15 +0700

    first commit

--- components/Icons/HistoryIcon.tsx
+++ components/Icons/HistoryIcon.tsx
@@ -4,8 +4,8 @@ function HistoryIcon({ fill = "#6C7281", ...rest }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg"
         width="30" height="30" viewBox="0 0 24 24" 
-        fill="none" stroke="#990012" stroke-width="2" 
-        stroke-linecap="round" stroke-linejoin="round">
+        fill="none" stroke="#990012" strokeWidth="2" 
+        strokeLinecap="round" strokeLinejoin="round">
         <circle cx="12" cy="12" r="10"></circle>
         <polyline points="12 6 12 12 16 14"></polyline>
     </svg>
