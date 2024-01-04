1802baeec3aeb09067f6b3fbc76b1cedd59e8fce
Hồ Thanh Nhã <127134736+hothanhnha256@users.noreply.github.com>
2024-01-04 08:38:49 +0700

    Update orderhistory.tsx

--- pages/orderhistory.tsx
+++ pages/orderhistory.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NextPage } from 'next';
-import Order from '../components/OrderHistory/Order';
+import Report from '../components/ReportStatistic/Reportrun';
 
-const OrderHistory: NextPage = () => {
+const ReportStatistic: NextPage = () => {
   return (
-    <div className="rounded-xl h-5/6">
-    <div className="h-full bg-gradient-to-b from-gray-100 to-gray-300 rounded-xl overflow-y-scroll content-center mt-10">
-        <Order/>
+    <div className="rounded-xl h-full w-full ">
+      <div className="h-[calc(95%)] bg-gradient-to-b from-gray-100 to-gray-300 rounded-xl  content-center mt-10">
+        <Report/>
       </div>
     </div>
   );
-};
-
-export default OrderHistory;
+}
+export default ReportStatistic;
