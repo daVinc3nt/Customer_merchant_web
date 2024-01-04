ad3f689ff31564d1d5207f15c26eb49e3fb09932
Hồ Thanh Nhã <127134736+hothanhnha256@users.noreply.github.com>
2024-01-04 22:03:23 +0700

    Update TimeDropDown.tsx

--- components/OrderHistory/TimeDropDown.tsx
+++ components/OrderHistory/TimeDropDown.tsx
@@ -15,14 +15,20 @@ const TimeDropDown = () => {
 
   return (
     <div className="relative p-4 z-20 ">
-      <button onClick={() => setIsOpen(!isOpen)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 sm:w-32 sm:h-10 rounded-full">
-        <span>Thời gian</span>
+      <button onClick={() => setIsOpen(!isOpen)} className="
+      bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-2
+      w-16 h-8
+      sm:w-32 sm:h-10 
+      rounded-full
+      flex items-center justify-center
+      ">
+        <span className='text-xs'>Thời gian</span>
       </button>
       {isOpen && (
         <div className="absolute mt-2 space-y-2 bg-white text-black border border-gray-200 rounded shadow-lg ">
           <div className='mx-2 my-2'>
             <input type="radio" id="1hour" name="time" value="1 giờ trước" onChange={handleTimeChange} />
-            <label htmlFor="1hour"> 1 giờ trước</label>
+            <label htmlFor="1hour"> 1 ngày trước</label>
           </div>
           <div className='mx-2 my-2'>
             <input type="radio" id="1week" name="time" value="1 tuần trước" onChange={handleTimeChange} />
@@ -31,6 +37,10 @@ const TimeDropDown = () => {
           <div className='mx-2 my-2'>
             <input type="radio" id="1month" name="time" value="1 tháng trước" onChange={handleTimeChange} />
             <label htmlFor="1month"> 1 tháng trước</label>
+          </div>
+          <div className='mx-2 my-2'>
+            <input type="radio" id="1month" name="time" value="1 tháng trước" onChange={handleTimeChange} />
+            <label htmlFor="1month"> Tất cả</label>
           </div>
           <div className="flex justify-end space-x-2 mt-2 ">
             <button onClick={() => setIsOpen(false)} className="px-4  text-white bg-red-500 rounded hover:bg-red-400">
