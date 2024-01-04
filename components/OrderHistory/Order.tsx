9d37d4b9c14391a56f7e393163ce3ddbf7e47cc8
Hồ Thanh Nhã <127134736+hothanhnha256@users.noreply.github.com>
2024-01-04 08:43:13 +0700

    Update Order.tsx

--- components/OrderHistory/Order.tsx
+++ components/OrderHistory/Order.tsx
@@ -48,17 +48,18 @@ const App = () => {
     <div >
         <div >
         <div className="font-sans text-black antialiased rounded-xl">
-        <nav className="flex items-center justify-between flex-wrap bg-white p-6 rounded-xl">
-      <div className="flex items-center flex-shrink-0 text-black mr-6">
-        <span className="font-semibold text-xl tracking-tight">Báo cáo thống kê</span>
-      </div>
-      <div className="block lg:hidden">
-        <button onClick={toggleNav} className="flex items-center px-3 py-2 border rounded text-black border-teal-400 hover:text-red-500 focus:text-red-500 hover:border-white">
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </button>
-      </div>
-      <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'}`}>
-        <div className="text-sm lg:flex-grow">
+        <nav className="justify-between flex-wrap bg-white p-6 rounded-xl">
+        <div className="flex justify-between flex-shrink-0 text-black mr-6">
+            <span className="font-semibold text-xl tracking-tight">Báo cáo thống kê</span>
+            <div className=" block lg:hidden">
+            <button onClick={toggleNav} className=" flex items-center px-3 py-2 border rounded text-black border-teal-400 hover:text-red-500 focus:text-red-500 hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+        </div>
+        </div>
+
+      <div className={` mt-3 w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'}`}>
+        <div className="text-sm lg:flex-grow ">
           {/* Your buttons go here */}
             <button onClick={() => setView('AllOrder')} className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-red-500 focus:text-red-500 mr-4">
                 Tất cả đơn hàng
