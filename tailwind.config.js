71af74fd89df6478ea9165fce0303be73a0a955d
Hồ Thanh Nhã <127134736+hothanhnha256@users.noreply.github.com>
2024-01-04 22:01:38 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
     extend: {
       fontSize: {
         'xxs': '0.5rem',
         '3xs': '0.25rem'
+      },
+      screens: {
+        xs : '360px',
+        s : '425px',
+
       },
       colors: {
         light: {
@@ -28,10 +34,28 @@ module.exports = {
         104: '26rem',
         112: '28rem',
         120: '30rem',
-        150: '37.5rem',
+        128: '32rem',
+        136: '34rem',
+        144: '36rem',
+        152: '38rem',
         160: '40rem',
-        170: '42.5rem',
+        168: '42rem',
+        176: '44rem',
+        184: '46rem',
+        192: '48rem',
         200: '50rem',
+         
+        'screen_1/6': '16.666667vh',
+        'screen_1/5': '20vh',
+        'screen_1/4': '25vh',
+        'screen_1/3': '33vh',
+        'screen_2/5': '40vh',
+        
+        'screen_1/2': '50vh',
+        'screen_3/5': '60vh',
+        'screen_2/3': '66vh',
+        'screen_3/4': '75vh',
+        'screen_4/5': '80vh',
       }
     },
   },
