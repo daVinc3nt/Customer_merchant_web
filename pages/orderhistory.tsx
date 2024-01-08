ed3ae1984ecda2e2c3b89def37ab344b6f7f56c7
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2024-01-08 20:07:34 +0700

    Update Reponsive

--- pages/orderhistory.tsx
+++ pages/orderhistory.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import { NextPage } from 'next';
-import Order from '../components/OrderHistory/Order';
+import React from "react";
+import { NextPage } from "next";
+import Order from "../components/OrderHistory/Order";
 
 const OrderHistory: NextPage = () => {
-  return (
-    <div className="rounded-xl h-full w-full">
-    <div className="h-[calc(93%)] bg-gradient-to-b from-gray-100 to-gray-300 rounded-2xl content-center mt-10">
-        <Order/>
-      </div>
-    </div>
-  );
+  return <Order />;
 };
 
 export default OrderHistory;
--- pages/reportpage.tsx
+++ pages/reportpage.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { NextPage } from 'next';
-import Report from '../components/ReportStatistic/Reportrun';
+import React from "react";
+import { NextPage } from "next";
+import Report from "../components/ReportStatistic/Reportrun";
 
 const ReportStatistic: NextPage = () => {
-  return (
-    <div className="rounded-xl h-full w-full ">
-      <div className="h-[calc(93%)] bg-gradient-to-b from-gray-100 to-gray-300 rounded-xl  content-center mt-10">
-        <Report/>
-      </div>
-    </div>
-  );
-}
+  return <Report />;
+};
 export default ReportStatistic;
